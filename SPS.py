8f14d5f595734b80f674cb2c215f952fe9e92993
Ronan Drost <ronandrost20@gmail.com>
2020-12-29 22:46:02 +0100

    shit

---
+++ SPS.py
@@ -0,0 +1,12 @@
+import random
+from tkinter import *
+
+root = Tk()
+root.configure(bg="#000000")
+root.geometry('+0+0')
+root.iconbitmap("favicon.ico")
+root.title("Ronan Drost Periode 2 python project")
+root.resizable(width=False, height=False)
+
+# Hand Foto's
+rockHandPhoto = PhotoImage(file="")
